cc4084875f3c9514074d266d13eb49bc8cca8b1c
Alex Mihailov <av.mihailov.dev@gmail.com>
2022-05-10 23:15:25 +0300

    Initial commit

---
+++ src/main/java/ru/witcher/MainServlet.java
@@ -0,0 +1,23 @@
+package ru.witcher;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>Hello Servlet</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
