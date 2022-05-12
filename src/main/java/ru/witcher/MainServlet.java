019fd5a077a2b1681b915f3fafab8b20bb8c2fa2
Alex Mihailov <av.mihailov.dev@gmail.com>
2022-05-12 22:01:46 +0300

    Построение war без web.xml и регистрация сервелета используя аннотация WebServlet

--- src/main/java/ru/witcher/MainServlet.java
+++ src/main/java/ru/witcher/MainServlet.java
@@ -1,12 +1,14 @@
 package ru.witcher;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet("/servlet-example/")
 public class MainServlet extends HttpServlet {
 
     @Override
